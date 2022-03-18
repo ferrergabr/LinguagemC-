d080ac1f1defe876faa0d621fd1e07fc465f7626
ferrergabr <ferrergabr78@gmail.com>
2022-03-18 09:43:39 -0300

    ano bissexto e tabuada

---
+++ 5-17032022/tabuada.c
@@ -0,0 +1,21 @@
+/*Programa para calcular a tabuada de 
+acordo com a entrada de usuário
+*/
+#include <stdio.h>
+int main(){
+    int n; 
+    int mult; 
+    printf("Digite um número a para a tabuada");
+    scanf("%d",&n);
+
+
+    for(mult = 0 ; mult <= 10 ; mult++){
+    printf("%d x %d = %d\n",n,mult,(n*mult));
+    }
+
+    
+    
+
+    
+    return 0; 
+}
