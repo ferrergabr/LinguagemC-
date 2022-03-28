393eae7b3aef10473f95ad0824fa5ad360128258
ferrergabr <ferrergabr78@gmail.com>
2022-03-28 11:07:42 -0300

    funçoes

---
+++ 10-28032022/funcoes1.c
@@ -0,0 +1,68 @@
+/*
+Programa para trabalhar com funções. Vamos usar funções com 
+retorno, sem retorno, com argumentos(parâmetros), sem argumentos
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+void separadorHT(){
+    for(int i = 0; i <= 50 ; i++){
+        printf("#");
+    }
+    printf("\n");
+}
+void separadorLinha(){
+    printf("\n");
+    for(int i = 0 ; i <= 50 ; i++){
+        printf("_");
+    }
+
+        printf("\n");
+    }
+
+
+    void pularLinha(int qtdVezes){
+        for(int i = 1 ; i <= qtdVezes; i++){
+            printf("\n");
+        }
+
+    }  
+
+
+   void comparar(int v1, int v2){
+    if(v1 > v2){
+        printf("O primeiro número é maior que o segundo");
+    }   
+    else if(v1==v2){
+    printf("Os dois números sao iguais");
+    }
+    else{
+        printf("O segundo número é maior que o primeiro");
+    }
+}
+   
+
+
+void main(){
+    int n1,n2;
+    system("clear");//limpar a tela do terminal
+    //chamando a função separadorHT
+    separadorHT();
+    pularLinha(5);
+    printf("\n\t\tPrograma que exibe uma mensagem\n");
+    pularLinha(10);
+    //chamando a função separadorLinha
+    separadorLinha();
+    pularLinha(2);
+    printf("Digite um número:\n");
+    scanf("%d",&n1);
+    printf("Digite outro número:\n");
+    scanf("%d",&n2);
+
+    
+    pularLinha(1);
+
+    comparar(n1,n2);
+
+    pularLinha(2);
+}
