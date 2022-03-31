b2359bea4f59886c43c9d3827cbad10a6b115aa0
Gabriel Ferrer <101403224+ferrergabr@users.noreply.github.com>
2022-03-31 11:11:35 -0300

    Add files via upload

---
+++ 11-31032022/calculos.h
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void soma(int array[], int tamanho){
+    int rs = 0;
+    for(int i = 0; i < tamanho ; i++){
+        rs += array[i];
+    }
+    printf("%d\n",rs);
+}
+
+void maiorValor(int array[],int tamanho){
+    int m = array[0];    
+    for(int i = 1 ; i < tamanho ; i++)
+    if(array[i] > m){
+        m = array[i];
+    } 
+    printf("O maior valor é %d",m);
+
+}
+
+
